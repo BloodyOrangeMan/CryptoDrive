9216fc89df74ca01a31b9cb7e37e6fb8d9a7b3d2
excuses0217 <lixiongfei0217@gmail.com>
2022-08-14 00:39:12 +0800

    Update Devteam

--- frontend/src/components/DevTeam.js
+++ frontend/src/components/DevTeam.js
@@ -8,10 +8,10 @@ export const DevTeam = [
 	},
 	{
 		id: 1,
-		name: 'BloodyOrangeMan',
-		imgUrl: 'https://avatars.githubusercontent.com/u/74171212?v=4',
-		githubUrl: 'https://github.com/BloodyOrangeMan',
-		linkedInUrl: 'https://www.linkedin.com/in/%E5%8F%8C%E6%9D%B0-%E9%BE%99-647b30243/',
+		name: 'excuses0217',
+		imgUrl: 'https://avatars.githubusercontent.com/u/74172902?s=400&u=2bf5f570120cc5f3180bdfc307a39a04b12c8b3f&v=4',
+		githubUrl: 'https://github.com/excuses0217',
+		linkedInUrl: 'https://www.linkedin.cn/incareer/in/%E9%9B%84%E9%A3%9E-%E6%9D%8E-302995248',
 	},
 	{
 		id: 2,
